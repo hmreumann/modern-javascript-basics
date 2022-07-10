84f456150ac6ace58b1f550b0ccd69bcc2b15113
Hernan Martinez Reumann <hmreumann@hotmail.com>
2022-07-10 09:37:16 -0300

    Lesson 5 - Promises are made

--- main.js
+++ main.js
@@ -1,71 +1,32 @@
 'use strict';
 
-// const user = {
-//     name: 'Hernán',
-//     email: 'hmreumann@hotmail.com'
-// };
-
-// const nameString = name => `${name} (${user.email})`;
-
-// document.getElementById('output').innerHTML = nameString('Hernán');
-
-let output = document.getElementById('output')
-
-// const users = [
-//     {
-//         name: 'Lua',
-//         email: 'lua@gmail.com',
-//     },
-//     {
-//         name: 'Lisa',
-//         email: 'lisa@gmail.com',
-//     }
-// ];
-
-// let names = [];
-
-// users.forEach(user => names.push(user.name))
-
-// output.innerHTML = names.join(', ');
-
-// document.getElementById('btn').addEventListener('click', () => {
-//     const buttonId = () => event.target.getAttribute('id')
-
-//     document.getElementById('output').innerHTML = `The buttons id is: ${buttonId()}`
-// })
-
-class Item{
-    static maxQuantity = 10
-    static getText(){
-        return `Something interesting to be said`
+let myPromise = new Promise((resolve, reject) => {
+    let user = {
+        name: 'Hernan',
+        email: 'hernan@gmail.com'
     }
 
-    constructor(name, category){
-        this.name = name
-        this.category = category
-    }
+    setTimeout(() => {
+        resolve(user)
+        // reject('We had an error')
+    }, 3000)
+})
 
-    getDetails(){
-        return `${this.name} - ${this.category} `
-    }
+let getAdditionalDetails = user => {
+    return new Promise((resolve, reject) => {
+        document.getElementById('output').innerHTML = `${user.name} - ${user.email}`
+
+        user.favoriteColor = 'Red'
+
+        setTimeout(() => {
+            resolve(user)
+        }, 3000)
+    })
 }
 
+myPromise
+    .then(getAdditionalDetails)
+    .then((user) => document.getElementById('output').innerHTML = `${user.name} - ${user.email} - ${user.favoriteColor}`)
+    .catch((error) => document.getElementById('output').innerHTML = error)
 
-class PurchasedItem extends Item {
-    constructor(name, category, price){
-        super(name, category)
-        this.price = price
-    }
-
-    getDetailsWithPrice(){
-        return `${this.name} - ${this.category} - $${this.price}`
-    }
-
-    static getPurchasedItems(){
-        return `3 / ${super.maxQuantity}`
-    }
-}
-
-let purchasedItem = new PurchasedItem('Cofee', 'Drinks', 1.95);
-
-output.innerHTML = purchasedItem.getDetailsWithPrice() + ` - ${PurchasedItem.getPurchasedItems()}` + ` - ${Item.getText()}`;
+document.getElementById('output').innerHTML = 'Waiting for the promise response.'
