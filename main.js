a94e15fcdaade4af904f79f1afcfbcdb52c7a261
Hernan Martinez Reumann <hmreumann@hotmail.com>
2022-07-12 21:18:55 -0300

    Lesson 9 Demystifying Modules

--- main.js
+++ main.js
@@ -1,55 +1,10 @@
 'use strict';
 
+import {users, currentUser} from './users.js'
+import toUpper from './helper.js'
+
 let output = document.getElementById('output')
 
-let fruits = [
-    'Manzana',
-    'Naranja',
-    'Banana',
-    'Limon',
-    'Melon',
-    'Cereza',
-    'Frambuesa'
-]
-
-// let fruitsThatContainE = []
-
-// fruits.forEach(function(fruit){
-//     if(fruit.includes('e')){
-//         fruitsThatContainE.push(fruit)
-//     }
-// })
-
-let fruitsThatContainE = fruits.filter(f => f.includes('e'))
-
-output.innerHTML = fruitsThatContainE.join(', ')
-
-// Reduce example
-
-let users = [
-    {
-        name: 'Hernán',
-        email: 'hmreumann@hotmail.com',
-        stars: 4
-    },
-    {
-        name: 'Luis',
-        email: 'lmreumann@hotmail.com',
-        stars: 9
-    },
-    {
-        name: 'Estefi',
-        email: 'tefi@hotmail.com',
-        stars: 85
-    },
-]
-
-let totalStars = users.reduce((stars, user) => stars += user.stars, 0)
-
-output.innerHTML += `<br> ${totalStars}`
-
-// Map Example
 
 let userEmails = users.map(u => u.email)
-
-output.innerHTML += `<br>Emails: ${userEmails.join(', ')}`
+output.innerHTML += toUpper(userEmails).join(', ')
---
+++ users.js
@@ -0,0 +1,21 @@
+const users = [
+    {
+        name: 'Hernán',
+        email: 'hmreumann@hotmail.com',
+        stars: 4
+    },
+    {
+        name: 'Luis',
+        email: 'lmreumann@hotmail.com',
+        stars: 9
+    },
+    {
+        name: 'Estefi',
+        email: 'tefi@hotmail.com',
+        stars: 85
+    },
+]
+
+const currentUser = 2
+
+export { users, currentUser }
